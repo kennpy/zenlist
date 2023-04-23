2094f7f4af51ce0e2d5d962f7c464ed47cabc09e
kennpy <64108774+kennpy@users.noreply.github.com>
2023-04-22 18:24:08 -0600

    test

--- api/index.js
+++ api/index.js
@@ -37,14 +37,14 @@ export default (req, res) => {
             fs.readFileSync(filepath, 'utf8', (err, data) => {
             if (err) {
             console.error(err);
-            res.send(currentDir)
+            res.send(err)
             //res.send(err.message, err.code, err.stack);
             }
             res.send(data);
         });    
         }
         catch (error) {
-            res.send(currentDir)
+            res.send(err)
         }
 
     //   res.status(200).json({ message: 'You submitted a get (POST) request!'})
