610d7b8e2dca30cefc0af4ffd8cdd0539f1edc43
kennpy <64108774+kennpy@users.noreply.github.com>
2023-04-22 18:04:13 -0600

    test

--- api/index.js
+++ api/index.js
@@ -20,20 +20,29 @@ export default (req, res) => {
         //const body = JSON.parse(req.body);
         console.log(currentDir);
         //res.status(200).json({ message: `You submitted a post request: ${body}` })
-        try{
-            fs.readFile('./storage/tasks.txt', 'utf8', (err, data) => {
-            if (err) {
-            console.error(err);
-            res.send(err.message, err.code, err.stack);
-            }
-            res.send(data);
-        });    
-        }
-        catch (error) {
-            res.send(error)
-        }
+        let myLog = new File("./storage/tasks.txt");
 
-      res.status(200).json({ message: 'You submitted a get (POST) request!'})
+        // See if the file exists
+        if(myLog.exists()){
+            res.send('The file exists');
+        }
+        else{
+            res.send("File does not exist")
+        }
+    //     try{
+    //         fs.readFile('./storage/tasks.txt', 'utf8', (err, data) => {
+    //         if (err) {
+    //         console.error(err);
+    //         res.send(err.message, err.code, err.stack);
+    //         }
+    //         res.send(data);
+    //     });    
+    //     }
+    //     catch (error) {
+    //         res.send(error)
+    //     }
+
+    //   res.status(200).json({ message: 'You submitted a get (POST) request!'})
 
   }
 }
