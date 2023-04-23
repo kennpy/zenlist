baf456f3c15268460dbc834fbdf362b076535dc0
kennpy <64108774+kennpy@users.noreply.github.com>
2023-04-22 19:30:39 -0600

    test

--- api/task/index.js
+++ api/task/index.js
@@ -23,7 +23,7 @@ export default (req, res) => {
             console.error(err);
           }
           console.log("added new task")
-          res.status(200)
+          res.send("added task")
   // file written successfully
         });
         break
