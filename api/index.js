882089984177eb232e35b428b1635923e66a9874
kennpy <64108774+kennpy@users.noreply.github.com>
2023-04-22 19:44:03 -0600

    test

--- api/index.js
+++ api/index.js
@@ -39,12 +39,12 @@ export default async (req, res) => {
                     if (err) {
                         const defaultTask = '{"test":[false,"978a8058-6376-4c80-86a1-9697be3b4509",0]}{"test":[false,"1a1326f2-c60d-4df1-ba92-65352b49c64c",0]}';
                         fs.appendFile("/tmp/tasks.txt", defaultTask, (newErr) => console.log("error :",newErr));
-                        fs.readFile("/tmp" + "/tasks.txt", 'utf8', (err, updatedErr) => {
+                        fs.readFile("/tmp" + "/tasks.txt", 'utf8', (updatedErr, updatedData) => {
                             if (updatedErr) {
                                 console.error(updatedErr);
                                 res.send(updatedErr);
                             }
-                        //res.send(err.message, err.code, err.stack);
+                            res.send(updatedData);
                         })
                     console.error(err);
                 //res.send(err.message, err.code, err.stack);
